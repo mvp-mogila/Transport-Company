a759cbdc1eb2e949efc4c78a79651e76c3c4d98e
Artem-Mo <m0gilevskiy@yandex.ru>
2023-11-21 15:05:02 +0300

    / handler

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #! /bin/python3
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
@@ -8,8 +8,8 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def default_handler():
-    return render_template('index.html')
+    return render_template('index.html', return_page_url=None)
 
 
 if (__name__ == '__main__'):
-    app.run(host = '127.0.0.1', port = 5000)
+    app.run(host = '127.0.0.1', port = 5000, debug=True)
