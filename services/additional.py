750e4935967048fcb0584f0842a6ec50bb3ab80d
Artem-Mo <m0gilevskiy@yandex.ru>
2023-12-07 00:07:26 +0300

    depricated

--- services/additional.py
+++ services/additional.py
@@ -3,7 +3,3 @@ def create_rows(dict: dict, param: str):
     for row in dict:
         result.append(row[param])
     return result
-
-
-def convert_date(date: str):
-    return 'STR_TO_DATE(' + date + ', "%Y-%m-%d")'
