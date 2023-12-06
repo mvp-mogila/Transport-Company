b9039485f7404591e11d7114b29fb2cbff72750f
Artem-Mo <m0gilevskiy@yandex.ru>
2023-12-06 22:42:50 +0300

    client views fixes

--- views/client_views.py
+++ views/client_views.py
@@ -1,9 +1,10 @@
+from http.client import BAD_REQUEST, NOT_FOUND
 from flask import Blueprint, request, render_template, session
+from werkzeug.exceptions import BadRequest, NotFound
 
 from services.access_control import login_required
 import managers.delivery_manager as delivery
-# import managers.user_manager as user
-# import managers.transport_manager as transport
+import managers.user_manager as user
 
 
 deliveries_app = Blueprint('deliveries_app', __name__, template_folder="templates")
@@ -12,62 +13,39 @@ deliveries_app = Blueprint('deliveries_app', __name__, template_folder="template
 @deliveries_app.route('/', methods=["GET"])
 @login_required
 def deliveries_handler():
+    delivery_id = request.args.get('delivery_id')
     weight_lower = request.args.get('weight_lower')
     weight_upper = request.args.get('weight_upper')
     status = request.args.get('status')
 
     user_id = session.get('user_id')
 
-    deliveries, response_status = delivery.get_user_deliveries(user_id, 
-                                            {'weight_lower': weight_lower,
-                                              'weight_upper': weight_upper,
-                                              'status': status})
-    if (response_status == 400):
-        return "<center><h1>Некорректный запрос</h1></center>", response_status
+    deliveries, response_code = delivery.get_user_deliveries( { 'user_id': user_id,
+                                        'delivery_id': delivery_id, 'status': status,
+                                        'weight_lower': weight_lower, 'weight_upper': weight_upper})
+    if (response_code == BAD_REQUEST):
+        raise BadRequest
 
-    search_options = [{'name': "Вес (от)", 'params': None, 'arg': 'weight_lower'},
-                      {'name': "Вес (до)", 'params': None, 'arg': 'weight_upper'},
-                      {'name': "Статус", 'params': ["Завершен", "В работе", "Отменен"], 'arg': 'status'}]
+    form_method = "GET"
+    button_title = "Поиск"
+    search_options = [ {'name': "Номер заказа", 'params': None, 'arg': 'delivery_id', 'type': 'number'},
+                       {'name': "Вес (от)", 'params': None, 'arg': 'weight_lower', 'type': 'number'},
+                       {'name': "Вес (до)", 'params': None, 'arg': 'weight_upper', 'type': 'number'},
+                       {'name': "Статус", 'params': ["Завершен", "В работе", "Отменен"], 'arg': 'status', 'type': None} ]
 
     return render_template('deliveries.html', all=True, user_deliveries=deliveries, 
-                           search_options=search_options, return_page_url='/', staff=False, logged=True), response_status
+            form_method=form_method, options=search_options, button_title=button_title, staff=False, logged=True), response_code
 
 
-# @login_required
-# @deliveries_app.route('/<int:delivery_id>')
-# def delivery_handler(delivery_id):
-#     user_id = 1  # session
+@login_required
+@deliveries_app.route('/<int:delivery_id>')
+def delivery_handler(delivery_id):
+    user_id = session.get('user_id')
+    params = {'user_id': user_id, 'delivery_id': delivery_id}
+    print(params)
+    delivery_details, response_code = delivery.get_user_delivery(params)
 
-#     delivery_details, response_status = delivery.get_delivery_info(
-#         delivery_id, user_id)
+    if (response_code == NOT_FOUND):
+        raise NotFound
 
-#     if (response_status == 400):
-#         return "<center><h1>Некорректный запрос</h1></center>", response_status
-
-#     # if (response_status == 200):
-#     #     client_info = user.get_user_fullname(user_id)
-#     #     manager_info = user.get_user_fullname(delivery_datils[''])
-#     #     driver_info = user.get_user_fullname(delivery_datils[''])
-#     #     transport_info = transport.get_transport_info(delivery_dateils[''])
-
-#     return render_template('deliveries.html', all=False, delivery=delivery_details, 
-#                            client=client_info, driver=driver_info, manager=manager_info, 
-#                            transport=transport_info, return_page_url='/delivery/'), response_status
-
-# @login_required
-# @deliveries_app.route('/new', methods=["GET", "POST"])
-# def new_delivery_handler():
-#     if (request.method == 'POST'):
-#         send_addr = request.args.get('send_address')
-#         deliv_addr = request.args.get('delivery_address')
-#         send_date = request.args.get('send_date')
-#         deliv_date = request.args.get('delivery_date')
-#         items = request.args.getlist('items')
-
-#     # deliveries, response_status = delivery.create_delivery( user_id, { 'send_date': send_date,
-#     #                                           'delivery_date': deliv_date,
-#     #                                           'weight_lower': weight_lower,
-#     #                                           'weight_upper': weight_upper,
-#     #                                           'status': status } )
-
-#     return render_template('new-delivery.html', return_page_url='/delivery/')
+    return render_template('delivery-details.html', delivery=delivery_details), response_code
