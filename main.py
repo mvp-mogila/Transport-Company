469eaeda141042b40aaecaebc0cbf96d1debcf3a
Artem-Mo <m0gilevskiy@yandex.ru>
2023-12-06 21:15:10 +0300

    error handlers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #! /bin/python3
 
-from flask import Flask, render_template, session
+from http.client import BAD_REQUEST, NOT_FOUND, FORBIDDEN
+from flask import Flask, render_template, session, request
+from werkzeug.exceptions import BadRequest, NotFound, Forbidden
 from os import urandom
 
 from views.client_views import deliveries_app
@@ -14,6 +16,25 @@ app.register_blueprint(deliveries_app, url_prefix='/delivery')
 app.register_blueprint(auth_app, url_prefix='/auth')
 app.register_blueprint(profile_app, url_prefix='/profile')
 app.register_blueprint(staff_app, url_prefix='/staff')
+
+
+@app.errorhandler(BadRequest)
+def handle_bad_request(e):
+    title = f'{e.code} Некорректный запрос'
+    return render_template('error.html', title=title, return_url=request.referrer), BAD_REQUEST
+
+
+@app.errorhandler(NotFound)
+def handle_bad_request(e):
+    title = f'{e.code} Не найдено'
+    return render_template('error.html', title=title, return_url=request.referrer), NOT_FOUND
+
+
+@app.errorhandler(Forbidden)
+def handle_forbidden(e):
+    title = f'{e.code} Доступ запрещен'
+    return render_template('error.html', title=title, return_url=request.referrer), FORBIDDEN
+
 
 @app.route('/')
 def default_handler():
