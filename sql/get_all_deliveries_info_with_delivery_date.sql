893b2693b0471bf17160ab4aa2d2c9418b433bf5
Artem-Mo <m0gilevskiy@yandex.ru>
2023-12-08 01:49:40 +0300

    left join changes

--- sql/get_all_deliveries_info_with_delivery_date.sql
+++ sql/get_all_deliveries_info_with_delivery_date.sql
@@ -5,9 +5,10 @@ DU.surname AS driver_surname,
 T.model AS transport_model
 FROM Invoice 
 JOIN Client C ON Invoice.client_id = C.doc_num JOIN User CU ON C.user_id = CU.id
-JOIN Staff M ON Invoice.manager_id = M.id JOIN User MU ON M.user_id = MU.id
-JOIN Staff D ON Invoice.driver_id = D.id JOIN User DU ON D.user_id = DU.id
-JOIN Transport T ON Invoice.transport_id = T.id
+LEFT JOIN Staff M ON Invoice.manager_id = M.id LEFT JOIN User MU ON M.user_id = MU.id
+LEFT JOIN Staff D ON Invoice.driver_id = D.id LEFT JOIN User DU ON D.user_id = DU.id
+LEFT JOIN Transport T ON Invoice.transport_id = T.id
 WHERE Invoice.id LIKE '$delivery_id'
 AND Invoice.delivery_date = STR_TO_DATE('$delivery_date', "%Y-%m-%d")
-AND Invoice.delivery_status LIKE '$status';
+AND Invoice.delivery_status LIKE '$status'
+ORDER BY Invoice.creation_date;
